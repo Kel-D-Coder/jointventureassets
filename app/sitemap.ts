b6275a3f14e58877de4f4d44aed3bfe90c8b7a97
Kel-D-Coder <okolochibundu887@gmail.com>
2025-12-31 23:36:53 +0100

    modified the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,16 @@
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.jointventureassets.com/',
+      url: 'https://www.jointventureassets.com',
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1
     },
     {
       url: 'https://www.jointventureassets.com/contact',
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8
     }
     // add more URLs here
   ];
